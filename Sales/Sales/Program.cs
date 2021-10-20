8f44ff054ffc3056121cc99e1a6ddd074c33f23a
Roisin-13 <roisin.wheeler@gmail.com>
2021-10-20 14:09:46 +0100

    data emtry method added

--- Sales/Sales/Program.cs
+++ Sales/Sales/Program.cs
@@ -11,30 +11,17 @@ namespace Sales
     {
         static void Main(string[] args)
         {
-            //============DB connection===================//
-            //---don't think I need this bad boy anymore - bye
+           
             MySqlConnection connection = mySqlUtils.GetConnection();
-            connection.Open();
-            //----connecting to sql file
-            mySqlUtils.RunSchema(Environment.CurrentDirectory + @"\static\schema.sql", connection);
-            //---checking connection
-            bool connectionOpen = connection.Ping();
-            Console.WriteLine($@"Connection status: {connection.State}
-            Ping successful: {connectionOpen}
-            DB version: {connection.ServerVersion}");
+            Menu menu = new Menu(new Controller(
+                    new Services(
+                        new Repository(connection))));
+            menu.firstMenu();
 
-            connection.Dispose();
-            //firstMenu();
 
         }
-        //public static void firstMenu()
-        //{
-        //    Menu menu = new Menu(new Controller(
-        //        new Services(
-        //            new Repository())));
-            
-        //    menu.firstMenu();
-        //}
+   
+       
 
     }
 }
--- Sales/Sales/Sales/Repository.cs
+++ Sales/Sales/Sales/Repository.cs
@@ -13,25 +13,37 @@ namespace Sales.Sales
     class Repository
     {
 
-
-        private IList<SaleModel> sales;
-        private static int counter = 0;
+        private readonly MySqlConnection connection;
+        //public MySqlConnection connection { get; }
+        public Repository(MySqlConnection mySqlConnection)
+        {
+            this.connection = mySqlConnection;
+        }
 
         //===========DATA ENTRY============//
         internal SaleModel Create(SaleModel toCreate)
         {
-           
+            connection.Open();
+
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "insert into sales(name, quantity, price, date_of_sale) " +
+                "values(@Name, @Quantity, @Price, @Date)";
+            command.Parameters.AddWithValue("@Name", toCreate.Name);
+            command.Parameters.AddWithValue("@Quantity", toCreate.Quantity);
+            command.Parameters.AddWithValue("@Price", toCreate.Price);
+            command.Parameters.AddWithValue("@Date", toCreate.Date);
+            command.Prepare();
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
             SaleModel sale = new SaleModel();
-            toCreate.ID = counter;
-            counter++;
+            sale.ID = (int)command.LastInsertedId;
             sale.Name = toCreate.Name;
             sale.Quantity = toCreate.Quantity;
             sale.Price = toCreate.Price;
             sale.Date = toCreate.Date;
             return sale;
-
-
-
 
 
 
